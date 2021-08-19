7a94fbb6326933d436e4669c932253ccacb79e79
danicalacuesta <darlingdanica12@gmail.com>
2021-08-19 16:43:50 -0400

    Enter

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -3,7 +3,10 @@ import React, { isValidElement } from 'react';
 const Todo = ({text,todo, todos, setTodos}) => {
     //Events
     const deleteHandler =()=> {
+        //^^delete button that modifies the state
         setTodos(todos.filter((el) => el.id !== todo.id)); 
+        //^^filter state out to find an element to whatever we clicked on (get rid of it if it matches)
+        //el=element
     };
 
     const completeHandler = () => {
@@ -12,16 +15,21 @@ const Todo = ({text,todo, todos, setTodos}) => {
             if(item.id===todo.id){
                 return{
                     ...item, completed: !item.completed
+                    //! flips property
                 };
             }
             return item;
+            //return state the way it was if it does not meet if statement
         })
       );
     };
 
     return (
+        //jsx code at bottom
+        //basic styling and text
         <div className="todo">
             <li className={`todo-item ${todo.completed ? "completed" :" "} `}>
+                //crosses off text when completed
                 {text}</li>
             <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check" ></i>
